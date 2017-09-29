59c8ebafaff2c8c7c225513f204e3b01628623e2
Tucker Bradford <tucker@tuckerbradford.com>
2017-09-29 19:22:53 -0400

    decided that increment_api_counter did not belong in authorize

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,5 +1,5 @@
 class API::V1::QuestionsController < ApplicationController
-  before_filter :authorize
+  before_filter :authorize, :increment_api_counter
 
   def index
     @questions = Question.public_questions
@@ -9,18 +9,20 @@ class API::V1::QuestionsController < ApplicationController
     @question = Question.public_questions.find( params[:id] )
   end
 
-  private
+ private
 
   def authorize
-    @tenant = Tenant.where(api_key: params[:api_key]).first
-    unless @tenant || use_basic_auth
+    unless api_key_from_query_params || api_key_from_basic_auth
       render json: {}, status: 401
       return false
     end
-    increment_api_counter
   end
 
-  def use_basic_auth
+  def api_key_from_query_params
+    @tenant = Tenant.where(api_key: params[:api_key]).first
+  end
+
+  def api_key_from_basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       @tenant = Tenant.find_by_api_key(password)
       @tenant.api_key == password
@@ -28,6 +30,7 @@ class API::V1::QuestionsController < ApplicationController
   end
 
   def increment_api_counter
+    authorize unless @tenant
     if @tenant
       @tenant.api_count += 1
       @tenant.save
