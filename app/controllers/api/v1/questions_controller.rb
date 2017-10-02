71b8f79870a461553afe49cd87c6a28ad6401c7e
Tucker Bradford <tucker@tuckerbradford.com>
2017-10-02 10:51:19 -0400

    After more reading, I think this approach is more secure. Ultimately though, Doorman…

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -12,20 +12,26 @@ class API::V1::QuestionsController < ApplicationController
  private
   def authorize
     unless api_key_from_query_params || api_key_from_basic_auth
-      sleep(rand(0.0009..0.0001))
       render json: {}, status: 401
       return false
     end
   end
 
   def api_key_from_query_params
-    @tenant = Tenant.where(api_key: params[:api_key]).first
+    if secure_compare(params[:api_key])
+      @tenant = Tenant.where(api_key: params[:api_key]).first
+      return true
+    end
+    return false
   end
 
   def api_key_from_basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      @tenant = Tenant.find_by_api_key(password)
-      @tenant.api_key == password
+      if secure_compare(password)
+        @tenant = Tenant.find_by_api_key(password)
+        return true
+      end
+      return false
     end
   end
 
@@ -35,6 +41,13 @@ class API::V1::QuestionsController < ApplicationController
       @tenant.api_count += 1
       @tenant.save
     end
+  end
+
+  def secure_compare(their_token)
+    tokens = Tenant.all.select(:api_key).map{|record| record.api_key}
+    tokens.shuffle.map{|our_token|
+      ActiveSupport::SecurityUtils.secure_compare(our_token, their_token) if their_token
+     }.include?(true)
   end
 
 end
