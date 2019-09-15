b0703f09198179ffbb17e9eb7667b6b413fb44a5
kbitner <kbitner76@gmail.com>
2019-09-15 13:41:11 -0500

    generate question and answer json

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,10 +11,24 @@ class QuestionsController < ApplicationController
     @tenants_count = Tenant.count
   end
 
+  # Endpoint to return all public questions and their associated answers
   def show
-    @questions = Question.all
+    questions = Question.public_question
+
+    questions_with_answers = questions.map do |question|
+      answers = Answer.where(question_id: question.id)
+      associated_answers = answers.map do |answer|
+        { id: answer.id, answer: answer.body }
+      end
+      { id: question.id, question: question.title, answers: associated_answers }
+    end
+
+    render json: { questions: questions_with_answers }.to_json
+  end
+
+  # Provide an endpoint to see ALL public questions
+  def all
+    @questions = Question.public_question
     render json: @questions
-    # @tenants = Tenant.all
-    # render json: @tenants
   end
 end
